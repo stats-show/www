f47a1e0a25c02b99cefa4242726f90eb7a201abf
Dmitry Zaets <dmitry.zaets@gmail.com>
2017-01-01 23:24:15 +0100

    Add website content

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.title-subdomain').typed({
+    strings: ['js', 'net', 'go'],
+    startDelay: 400,
+    backDelay: 2000,
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+  });
+});
